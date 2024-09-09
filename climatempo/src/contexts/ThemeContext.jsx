bdb9bac7d08a47d943e9a7a65a484ae74c66f03a
“janeckiisa” <janeckiisabelle@gmail.com>
2024-09-09 19:57:27 -0300

    Starting ThemeContext.

--- climatempo/src/contexts/ThemeContext.jsx
+++ climatempo/src/contexts/ThemeContext.jsx
@@ -1,4 +1,17 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const ThemeContext = createContext();
 
+export const ThemeContextProvider = ({children}) => {
+    const [icon, setIcon] = useState("");
+    const [bg, setBg] = useState("");
+
+    return(
+        <ThemeContext.Provider value={{
+            icon, setIcon,
+            bg, setBg
+        }}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
